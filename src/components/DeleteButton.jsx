e2f97af1e54ac42a3322f77c79671edff935dd4e
Agrey Tosira <agreytosira@gmail.com>
2024-02-01 00:36:18 +0700

    style: use react-icons

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,16 +1,17 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { FiDelete } from 'react-icons/fi';
 
 function DeleteButton({ id, onDelete }) {
-  return (
-    <button className='contact-item__delete' onClick={() => onDelete(id)}>
-      X
-    </button>
-  )
+    return (
+        <button className='contact-item__delete' onClick={() => onDelete(id)}>
+            <FiDelete />
+        </button>
+    );
 }
 
 DeleteButton.propTypes = {
-  id: PropTypes.number.isRequired,
-  onDelete: PropTypes.func.isRequired
-}
+    id: PropTypes.number.isRequired,
+    onDelete: PropTypes.func.isRequired
+};
 
-export default DeleteButton
+export default DeleteButton;
