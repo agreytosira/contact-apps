830b5c1da044426105f936f664eff6fcbb4d248b
Agrey Tosira <agreytosira@gmail.com>
2024-02-11 15:49:31 +0700

    feat: add login page

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -4,17 +4,46 @@ import Navigation from './Navigation';
 import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
 import RegisterPage from '../pages/RegisterPage';
+import LoginPage from '../pages/LoginPage';
+import { getUserLogged, putAccessToken } from '../utils/api';
 
 class ContactApp extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            authedUser: null
+            authedUser: null,
+            initializing: true
         };
+
+        this.onLoginSuccess = this.onLoginSuccess.bind(this);
+    }
+
+    async componentDidMount() {
+        const { data } = await getUserLogged();
+        this.setState(() => {
+            return {
+                authedUser: data,
+                initializing: false
+            };
+        });
+    }
+
+    async onLoginSuccess({ accessToken }) {
+        putAccessToken(accessToken);
+        const { data } = await getUserLogged();
+        this.setState(() => {
+            return {
+                authedUser: data
+            };
+        });
     }
 
     render() {
+        if (this.state.initializing) {
+            return null;
+        }
+
         if (this.state.authedUser === null) {
             return (
                 <div className='contact-app'>
@@ -23,7 +52,7 @@ class ContactApp extends React.Component {
                     </header>
                     <main>
                         <Routes>
-                            <Route path='/*' element={<p>Halaman Login</p>} />
+                            <Route path='/*' element={<LoginPage loginSuccess={this.onLoginSuccess} />} />
                             <Route path='/register' element={<RegisterPage />} />
                         </Routes>
                     </main>
