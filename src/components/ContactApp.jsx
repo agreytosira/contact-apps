549c40e67b4dbf36a2573f66de9b3d8aa0896d6c
Agrey Tosira <agreytosira@gmail.com>
2024-02-11 15:39:10 +0700

    feat: add register page

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -3,9 +3,34 @@ import { Route, Routes } from 'react-router-dom';
 import Navigation from './Navigation';
 import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
+import RegisterPage from '../pages/RegisterPage';
 
 class ContactApp extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            authedUser: null
+        };
+    }
+
     render() {
+        if (this.state.authedUser === null) {
+            return (
+                <div className='contact-app'>
+                    <header className='contact-app__header'>
+                        <h1>Aplikasi Kontak</h1>
+                    </header>
+                    <main>
+                        <Routes>
+                            <Route path='/*' element={<p>Halaman Login</p>} />
+                            <Route path='/register' element={<RegisterPage />} />
+                        </Routes>
+                    </main>
+                </div>
+            );
+        }
+
         return (
             <div className='contact-app'>
                 <header className='contact-app__header'>
